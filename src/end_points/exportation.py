5590723a432bbb23142b16538b3e1b02d9435fdc
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-06-05 12:23:12 +0200

    exportation

--- src/end_points/exportation.py
+++ src/end_points/exportation.py
@@ -33,10 +33,10 @@ def export_order_report(user: ItalcoUser, id):
     drc=orders[0]['drc'],
     customer=orders[0]['user'],
     addressee=orders[0]['addressee'],
-    products=', '.join(orders[0]['products']),
+    products=', '.join([product['name'] for product in orders[0]['products'].get('customer', [])]),
     collection_point=orders[0]['collection_point'],
     note=orders[0]['customer_note'] if 'customer_note' in orders[0] else '/',
-    services=', '.join([service['name'] for service in orders[0]['services']]),
+    services=', '.join([service['name'] for service in orders[0]['products'].get('customer', [])]),
   ), dest=result)
   if pisa_status.err:
     raise Exception('Errore nella creazione del PDF')
