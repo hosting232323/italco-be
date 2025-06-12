91a60887ec186c23247eb8e71cebf286926bdf5f
cl_cassanelli <cldevofficial@gmail.com>
2025-06-12 12:32:21 +0200

    push

--- src/end_points/exportation.py
+++ src/end_points/exportation.py
@@ -2,12 +2,14 @@ from io import BytesIO
 from xhtml2pdf import pisa
 from flask import Blueprint, render_template, make_response, request
 
+from database_api import Session
 from ..database.enum import UserRole
 from ..database.schema import ItalcoUser
 from .order import query_orders, format_query_result
 from . import error_catching_decorator, flask_session_authentication
-from .schedule import query_schedules, format_schedules_query_result
-
+# from .schedule import query_schedules, format_schedules_query_result
+from ..database.schema import Schedule, ItalcoUser, Order, DeliveryGroup, Transport, OrderServiceUser, ServiceUser, Service
+from .order import add_service
 
 export_bp = Blueprint('export_bp', __name__)
 
@@ -114,3 +116,47 @@ def export_orders_schedule(user: ItalcoUser, id):
   response.headers['Content-Type'] = 'application/pdf'
   response.headers['Content-Disposition'] = 'inline; filename=report.pdf'
   return response
+
+
+def query_schedules(id: int | None = None) -> list[tuple[Schedule, DeliveryGroup, Transport, Order, OrderServiceUser, ServiceUser, Service]]:
+  with Session() as session:
+    query = session.query(
+      Schedule, DeliveryGroup, Transport, Order, OrderServiceUser, ServiceUser, Service
+    ).join(
+      DeliveryGroup, Schedule.delivery_group_id == DeliveryGroup.id
+    ).join(
+      Transport, Schedule.transport_id == Transport.id
+    ).outerjoin(
+      Order, Order.schedule_id == Schedule.id
+    ).outerjoin(
+      OrderServiceUser, OrderServiceUser.order_id == Order.id
+    ).outerjoin(
+      ServiceUser, OrderServiceUser.service_user_id == ServiceUser.id
+    ).outerjoin(
+      Service, ServiceUser.service_id == Service.id
+    )
+    if id is not None:
+      query = query.filter(Schedule.id == id)
+
+    return query.all()
+
+ 
+def format_schedules_query_result(tupla: tuple[
+  Schedule, DeliveryGroup, Transport, Order, OrderServiceUser, ServiceUser, Service
+], list: list[dict]) -> list[dict]:
+  for element in list:
+    if element['id'] == tupla[0].id:
+      element['orders'].append(tupla[3].to_dict())
+      add_service(element, tupla[6], tupla[4], tupla[5].price)
+      return list
+    
+  output = {
+    **tupla[0].to_dict(),
+    'orders': [tupla[3].to_dict()],
+    'transport': tupla[2].to_dict(),
+    'delivery_group': tupla[1].to_dict()
+  }
+
+  add_service(output, tupla[6], tupla[4], tupla[5].price)
+  list.append(output)
+  return list
