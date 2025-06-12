92574d1a721638667cafae600f87cc1b0750f8fe
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-06-12 17:07:34 +0200

    fix di vanni

--- src/end_points/exportation.py
+++ src/end_points/exportation.py
@@ -7,8 +7,7 @@ from ..database.enum import UserRole
 from ..database.schema import ItalcoUser
 from .order import query_orders, format_query_result
 from . import error_catching_decorator, flask_session_authentication
-# from .schedule import query_schedules, format_schedules_query_result
-from ..database.schema import Schedule, ItalcoUser, Order, DeliveryGroup, Transport, OrderServiceUser, ServiceUser, Service
+from ..database.schema import Schedule, ItalcoUser, Order, DeliveryGroup, Transport, OrderServiceUser, ServiceUser, Service, CollectionPoint, Photo
 from .order import add_service
 
 export_bp = Blueprint('export_bp', __name__)
@@ -38,7 +37,7 @@ def export_order_report(user: ItalcoUser, id):
     customer=orders[0]['user'],
     address=orders[0]['address'],
     addressee=orders[0]['addressee'],
-    addressee_contact=orders[0]['addressee_contact'],
+    addressee_contact=orders[0].get('addressee_contact', '/'),
     products=orders[0]['products'],
     collection_point=orders[0]['collection_point'],
     note=orders[0].get('customer_note', '/')
@@ -86,27 +85,30 @@ def export_orders_invoice(user: ItalcoUser):
 @flask_session_authentication([UserRole.ADMIN])
 def export_orders_schedule(user: ItalcoUser, id):
   orders = []
-  for tupla in query_orders(user, [{
-    'model': 'Order',
-    'field': 'id',
-    'value': int(id)
-  }]):
-    orders = format_query_result(tupla, orders, user)
-  if len(orders) != 1:
-    raise Exception('Numero di ordini trovati non valido')
-  
-  schedules = []
-  for tupla in query_schedules(id):
-    schedules = format_schedules_query_result(tupla, schedules)
+  schedule = {}
+  for index, tupla in enumerate(query_schedule(id)):
+    if index == 0:
+      schedule = {
+        **tupla[0].to_dict(),
+        'transport': tupla[2].to_dict(),
+        'delivery_group': tupla[1].to_dict()
+      }
+    orders = format_query_result(tuple(
+      value for index, value in enumerate(tupla) if not index in [0, 1, 2]
+    ), orders, user)
+
+  print('_____')
+  print(schedule)
+  print('_____')
+  print(orders)
   
   result = BytesIO()
   pisa_status = pisa.CreatePDF(src=render_template(
     'schedules_report.html',
-    id=schedules[0]['id'],
-    date=schedules[0]['date'],
-    delivery_group=schedules[0]['delivery_group']['name'],
-    transport=schedules[0]['transport']['name'],
-    info=schedules[0]['orders'],
+    id=schedule['id'],
+    date=schedule['date'],
+    delivery_group=schedule['delivery_group']['name'],
+    transport=schedule['transport']['name'],
     orders=orders
   ), dest=result)
   if pisa_status.err:
@@ -118,10 +120,10 @@ def export_orders_schedule(user: ItalcoUser, id):
   return response
 
 
-def query_schedules(id: int | None = None) -> list[tuple[Schedule, DeliveryGroup, Transport, Order, OrderServiceUser, ServiceUser, Service]]:
+def query_schedule(id: int) -> list[tuple[Schedule, DeliveryGroup, Transport, Order, OrderServiceUser, ServiceUser, Service, ItalcoUser, CollectionPoint, Photo]]:
   with Session() as session:
     query = session.query(
-      Schedule, DeliveryGroup, Transport, Order, OrderServiceUser, ServiceUser, Service
+      Schedule, DeliveryGroup, Transport, Order, OrderServiceUser, ServiceUser, Service, ItalcoUser, CollectionPoint, Photo
     ).join(
       DeliveryGroup, Schedule.delivery_group_id == DeliveryGroup.id
     ).join(
@@ -129,34 +131,19 @@ def query_schedules(id: int | None = None) -> list[tuple[Schedule, DeliveryGroup
     ).outerjoin(
       Order, Order.schedule_id == Schedule.id
     ).outerjoin(
+      CollectionPoint, Order.collection_point_id == CollectionPoint.id
+    ).outerjoin(
       OrderServiceUser, OrderServiceUser.order_id == Order.id
     ).outerjoin(
       ServiceUser, OrderServiceUser.service_user_id == ServiceUser.id
     ).outerjoin(
       Service, ServiceUser.service_id == Service.id
+    ).outerjoin(
+      ItalcoUser, ServiceUser.user_id == ItalcoUser.id
+    ).outerjoin(
+      Photo, Photo.order_id == Order.id
+    ).filter(
+      Schedule.id == id
     )
-    if id is not None:
-      query = query.filter(Schedule.id == id)
 
     return query.all()
-
- 
-def format_schedules_query_result(tupla: tuple[
-  Schedule, DeliveryGroup, Transport, Order, OrderServiceUser, ServiceUser, Service
-], list: list[dict]) -> list[dict]:
-  for element in list:
-    if element['id'] == tupla[0].id:
-      element['orders'].append(tupla[3].to_dict())
-      add_service(element, tupla[6], tupla[4], tupla[5].price)
-      return list
-    
-  output = {
-    **tupla[0].to_dict(),
-    'orders': [tupla[3].to_dict()],
-    'transport': tupla[2].to_dict(),
-    'delivery_group': tupla[1].to_dict()
-  }
-
-  add_service(output, tupla[6], tupla[4], tupla[5].price)
-  list.append(output)
-  return list
