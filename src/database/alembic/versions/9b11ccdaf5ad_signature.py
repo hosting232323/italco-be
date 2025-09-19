977acc993d98f73c95f9da4b60840cf414b4fcf0
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-09-19 12:08:34 +0000

    Signature

---
+++ src/database/alembic/versions/9b11ccdaf5ad_signature.py
@@ -0,0 +1,24 @@
+"""signature
+
+Revision ID: 9b11ccdaf5ad
+Revises: 8d5691ba121c
+Create Date: 2025-09-17 15:36:01.454473
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+revision: str = '9b11ccdaf5ad'
+down_revision: Union[str, None] = '8d5691ba121c'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+  op.add_column('order', sa.Column('signature', sa.LargeBinary(), nullable=True))
+
+
+def downgrade() -> None:
+  op.drop_column('order', 'signature')
