33fdbc9a0a7f6ed1abad5489c8f9b0f11100697f
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-10-01 09:50:47 +0000

    Script

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,26 +1,17 @@
 import os
-import json
 from tqdm import tqdm
 
 from database_api import set_database
-from src.database.schema import Order
-from database_api.operations import update, get_by_id
-
-
-def read_file(file_path):
-  with open(file_path, 'r', encoding='utf-8') as file:
-    return json.load(file)
+from src.database.schema import Order, OrderStatus
+from database_api.operations import get_all, update
 
 
 if __name__ == '__main__':
   set_database(os.environ['DATABASE_URL'])
-  products = read_file('scripts/product.json')
-  for order_product in tqdm(read_file('scripts/order_product.json')):
-    order: Order = get_by_id(Order, order_product['order_id'])
-    if not order.products:
-      order.products = []
-    update(order, {
-      'products': order.products + [next((
-        product['name'] for product in products if product['id'] == order_product['product_id']
-      ), 0)]
-    })
+
+  orders: list[Order] = get_all(Order)
+
+  for order in tqdm(orders, desc='Aggiornamento delivery_date'):
+    if order.status in [OrderStatus.COMPLETED, OrderStatus.CANCELLED]:
+      if not order.booking_date:
+        update(order, {'booking_date': order.dpc})
