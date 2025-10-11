5b0c2dc10b9594f6da25ceb08db569ee9cb8cb2f
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-10-11 16:49:47 +0200

    remvoing swagger

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
-from flask_swagger_ui import get_swaggerui_blueprint
 
 from api import swagger_decorator
 from database_api.backup import db_backup
@@ -17,11 +16,6 @@ load_dotenv()
 IS_DEV = int(os.environ.get('IS_DEV', 1)) == 1
 DATABASE_URL = os.environ['DATABASE_URL']
 app = Flask(__name__, static_folder='../static', template_folder='../templates')
-
-# Swagger da eliminare?
-app.register_blueprint(
-  get_swaggerui_blueprint('/swagger', '/static/swagger.json', config={'app_name': 'Italco'}), url_prefix='/swagger'
-)
 
 if IS_DEV:
   CORS(app)
