322a2f47a9a604dca13c586bb9e9fcf04d6e2f13
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-10-14 17:24:04 +0200

    push

--- src/end_points/chatty.py
+++ src/end_points/chatty.py
@@ -1,9 +1,10 @@
 import os
 import openai
 from flask import request, Blueprint
+from datetime import datetime, timedelta
 
 from ..database.enum import UserRole
-from .orders.queries import query_orders
+from .orders.queries import query_orders, format_query_result
 from ..database.schema import ItalcoUser
 from . import flask_session_authentication
 
@@ -15,12 +16,20 @@ chatty_bp = Blueprint('chatty_bp', __name__)
 @chatty_bp.route('message', methods=['POST'])
 @flask_session_authentication([UserRole.ADMIN])
 def send_message(user: ItalcoUser):
+  today = datetime.now().date()
+  two_weeks_ago = today - timedelta(days=14)
+  for tupla in query_orders(user, [], {
+    'start_date': two_weeks_ago.strftime('%Y-%m-%d'),
+    'end_date': today.strftime('%Y-%m-%d')
+  }):
+    orders = format_query_result(tupla, orders, user)
+  
   response = openai.chat.completions.create(
     model='gpt-4o',
     messages=[
       {'role': 'system', 'content': 'Sei Chatty, l\'assistente di Italco.'},
       {'role': 'system', 'content': 'Ecco la lista aggiornata degli ordini dell\'utente:\n\n' +
-        ' - '.join([serialize_order(order) for order in query_orders(user, [])]) +
+        ' - '.join(order for order in orders) +
         '\n\nUsa queste informazioni per rispondere alle domande dell\'utente o aggiornarlo sullo stato dei suoi ordini.'},
       {'role': 'user', 'content': request.json['message']}
     ]
@@ -30,20 +39,3 @@ def send_message(user: ItalcoUser):
     'status': 'ok',
     'message': response.choices[0].message.content
   }
-  
-
-def serialize_order(order_tuple):
-  order, order_service_user, service_user, service, italco_user, collection_point = order_tuple
-  return (
-    f'Ordine ID: {order.id}\n'
-    f'Stato: {order.status.name}\n'
-    f'Tipo servizio: {service.type.name}\n'
-    f'Prodotto: {order_service_user.product}\n'
-    f'Prezzo: {service_user.price}€\n'
-    f'Indirizzo: {order.address}\n'
-    f'CAP: {order.cap}\n'
-    f'Data prevista consegna: {order.dpc}\n'
-    f'Ritardo: {"Sì" if order.delay else "No"}\n'
-    f'Anomalia: {"Sì" if order.anomaly else "No"}\n'
-    f'---'
-  )
