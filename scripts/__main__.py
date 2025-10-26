5d2f87366e82923589d4290f4609f5d64ef8512d
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-10-26 13:44:34 +0100

    fix schedule migration

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -12,37 +12,37 @@ DELIVERY_USER_MAP = {
   3: [17, 19],
   4: [6, 5],
   12: [19, 8],
-  6: [7], # Elmy??
+  6: [7, 27],
   25: [26, 6],
-  10: [], # Vulpo?? Addante??
-  18: [], # Vulpo??
+  10: [28, 29],
+  18: [28],
   20: [17, 26],
-  11: [7], # De Caro??
+  11: [7, 30],
   17: [5, 19],
-  15: [7], # Elmy??
-  13: [], # De Caro?? Abdullah??
+  15: [7, 27],
+  13: [30, 31],
   21: [], # Squadra consegne aggiuntive??
   5: [6, 8],
   19: [26, 8, 19],
   24: [], # Squadra 1??
-  8: [7], # Abdullah??
-  23: [], # De Caro?? Addante??
+  8: [7, 31],
+  23: [29, 30],
   1: [], # Squadra Trony Taranto??
-  22: [], # Raspanti?? Abdullah??
+  22: [31, 32],
   14: [5, 6],
   9: [5, 19],
-  7: [], # Abdullah?? Elmy??
+  7: [27, 31]
 }
 
 
 def read_schedule_by_file():
-  with open('dati.json', 'r', encoding='utf-8') as file:
+  with open('scripts/schedule.json', 'r', encoding='utf-8') as file:
     return json.load(file)
 
 
 if __name__ == '__main__':
   set_database(os.environ['DATABASE_URL'])
-  for schedule in tqdm(read_schedule_by_file, desc='Aggiornamento delivery group'):
+  for schedule in tqdm(read_schedule_by_file(), desc='Aggiornamento delivery group'):
     if schedule['delivery_group_id'] in DELIVERY_USER_MAP:
       if len(DELIVERY_USER_MAP[schedule['delivery_group_id']]):
         for user_id in DELIVERY_USER_MAP[schedule['delivery_group_id']]:
