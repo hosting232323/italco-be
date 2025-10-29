5380981fe4a4951ec1a560510bf169a09108e30b
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-10-29 11:39:06 +0100

    fix di vanni

---
+++ src/database/alembic/versions/aa850583320c_rescheduled.py
@@ -0,0 +1,37 @@
+"""Rescheduled
+
+Revision ID: aa850583320c
+Revises: 6e5bf22fe99a
+Create Date: 2025-10-27 12:33:08.295516
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = 'aa850583320c'
+down_revision: Union[str, None] = '6e5bf22fe99a'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+  op.execute("ALTER TYPE orderstatus ADD VALUE 'RESCHEDULED';")
+  op.add_column('order', sa.Column('id_cloned_order', sa.Integer(), nullable=True))
+
+
+def downgrade() -> None:
+  op.drop_column('order', 'id_cloned_order')
+  op.execute('ALTER TYPE orderstatus RENAME TO orderstatus_old;')
+  op.execute("""
+    CREATE TYPE orderstatus AS ENUM ('PENDING', 'IN_PROGRESS', 'ON_BOARD', 'COMPLETED', 'CANCELLED', 'AT_WAREHOUSE', 'TO_RESCHEDULE');
+  """)
+  op.execute("""
+    ALTER TABLE "order"
+    ALTER COLUMN status DROP DEFAULT,
+    ALTER COLUMN status TYPE orderstatus
+    USING status::text::orderstatus;
+  """)
+  op.execute('DROP TYPE orderstatus_old;')
