41713241f7d603be8e79bf7ede9751f587f4c493
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-11-30 17:14:43 +0100

    gabriele-fix

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,35 +1,70 @@
 import os
+import pandas as pd
 from tqdm import tqdm
+from sqlalchemy import and_
 
-from src.database.enum import UserRole
-from database_api.operations import create
 from database_api import set_database, Session
-from src.database.schema import DeliveryGroup, Schedule, User
+from src.database.schema import Order, Product, Service, ServiceUser
+from database_api.operations import create, get_by_id, delete
 
 
-def get_wrong_schedules() -> list[Schedule]:
+FILE_PATH = ''
+ID_ROW_LABEL = 'ID'
+PRODUCT_ROW_LABEL = 'Prodotto'
+SERVICE_ROW_LABEL = 'Servizio'
+CUSTOMER_USER_ID = ''
+COLLECTION_POINT_ID = ''
+
+
+def get_products(order: Order) -> list[Product]:
   with Session() as session:
-    return session.query(Schedule).outerjoin(
-      DeliveryGroup, Schedule.id == DeliveryGroup.schedule_id
-    ).filter(
-      DeliveryGroup.schedule_id == None
-    ).all()
+    return session.query(Product).filter(Product.order_id == order).all()
 
 
-def get_sample_user() -> User:
+def get_service_user(service_name: str) -> ServiceUser:
   with Session() as session:
-    return session.query(User).filter(
-      User.role == UserRole.DELIVERY,
-      User.nickname == 'delivery',
-    ).first()
+    return session.query(ServiceUser).join(Service, and_(
+      ServiceUser.user_id == CUSTOMER_USER_ID,
+      Service.name == service_name,
+      ServiceUser.service_id == Service.id
+    )).first()
 
 
 if __name__ == '__main__':
   set_database(os.environ['DATABASE_URL'])
-  delivery_user = get_sample_user()
-  for schedule in tqdm(get_wrong_schedules()):
-    print(schedule)
-    create(DeliveryGroup, {
-      'user_id': delivery_user.id,
-      'schedule_id': schedule.id
-    })
+  df = pd.read_excel(FILE_PATH, dtype=str).fillna('')
+  df.columns = [c.strip() for c in df.columns]
+  orders = {}
+  for _, row in df.iterrows():
+    id = int(row[ID_ROW_LABEL])
+    if id not in orders:
+      orders[id] = {row[PRODUCT_ROW_LABEL]: [row[SERVICE_ROW_LABEL]]}
+    elif row[PRODUCT_ROW_LABEL] not in orders[id]:
+      orders[id][row[PRODUCT_ROW_LABEL]] = [row[SERVICE_ROW_LABEL]]
+    elif row[SERVICE_ROW_LABEL] not in orders[id][row[PRODUCT_ROW_LABEL]]:
+      orders[id][row[PRODUCT_ROW_LABEL]].append(row[SERVICE_ROW_LABEL])
+    else:
+      print(f'Riga duplicata {id} {row[PRODUCT_ROW_LABEL]} {row[SERVICE_ROW_LABEL]}')
+
+  for order_id in tqdm(orders.keys()):
+    order = get_by_id(Order, order_id)
+    if not order:
+      print(f'Non trovato ordine con id {order_id}')
+      continue
+
+    for product in get_products(order):
+      delete(product)
+
+    for product in orders[order_id].keys():
+      for service_name in order[order_id][product]:
+        service = get_service_user(service_name)
+        if not service:
+          print(f'Non trovato servizio con nome {service_name}')
+          continue
+
+        create(Product, {
+          'name': product,
+          'order_id': order_id,
+          'service_user_id': service.id,
+          'collection_point_id': COLLECTION_POINT_ID
+        })
