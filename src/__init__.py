0b7abdf51ad23df1c33c81084699b7a9c321d30e
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-12-12 17:52:00 +0100

    fix di vanni

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,6 @@ import os
 from flask_cors import CORS
 from datetime import datetime
 from dotenv import load_dotenv
-from werkzeug.utils import secure_filename
 from flask import Flask, send_from_directory
 
 from database_api.backup import data_export
@@ -46,15 +45,16 @@ def index():
 @app.route('/internal-backup', methods=['POST'])
 def trigger_backup():
   zip_filename = data_export(DATABASE_URL)
-  safe_name = secure_filename(zip_filename)
-  
-  os.makedirs(STATIC_FOLDER, exist_ok=True)
-  dest_path = os.path.join(STATIC_FOLDER, safe_name)
-
-  os.rename(zip_filename, dest_path)
+  backup_path = os.path.join(STATIC_FOLDER, 'backup')
+  os.makedirs(backup_path, exist_ok=True)
+  os.rename(zip_filename, os.path.join(backup_path, zip_filename))
   manage_local_backups(STATIC_FOLDER)
-  
   print(f'[{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}] Backup eseguito!')
+
+
+@app.route('/<path:filename>')
+def serve_image(filename):
+  return send_from_directory(STATIC_FOLDER, filename)
 
 
 def manage_local_backups(local_folder: str):
@@ -63,17 +63,10 @@ def manage_local_backups(local_folder: str):
     for f in os.listdir(local_folder)
     if os.path.isfile(os.path.join(local_folder, f))
   ]
-    
   backups.sort()
-    
   backup_days = int(os.environ.get('POSTGRES_BACKUP_DAYS', 14))
 
   if len(backups) > backup_days:
     files_to_delete = backups[: len(backups) - backup_days]
     for path in files_to_delete:
       os.remove(path)
-
-
-@app.route('/<path:filename>')
-def serve_image(filename):
-  return send_from_directory(STATIC_FOLDER, filename)
