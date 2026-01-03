ebdd83238af1d3ef32cc9724a7ffbeb248e2e271
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-01-03 13:03:50 +0100

    photo-link

--- scripts/replace_photo.py
+++ scripts/replace_photo.py
@@ -4,19 +4,11 @@ from database_api.operations import update
 from database_api import set_database, Session
 
 
-OLD_DOMAIN = "https://ares-logistics.it"
-NEW_DOMAIN = "https://test.ares-logistics.it"
-
-
 def get_all_photos() -> list[Photo]:
   with Session() as session:
     return session.query(Photo).all()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
   set_database(DATABASE_URL)
-
   for photo in get_all_photos():
-    if photo.link and photo.link.startswith(OLD_DOMAIN):
-      new_link = photo.link.replace(OLD_DOMAIN, NEW_DOMAIN, 1)
-      update(photo, {"link": new_link})
+    update(photo, {'link': photo.link.replace('/api/', '/api/photos/')})
