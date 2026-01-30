ce1baa934f96ce47272a9e537b3291986a32a43b
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-01-30 16:25:24 +0100

    push

--- src/end_points/log.py
+++ src/end_points/log.py
@@ -11,37 +11,9 @@ log_bp = Blueprint('log_bp', __name__)
 @log_bp.route('', methods=['GET'])
 @flask_session_authentication([UserRole.ADMIN])
 def get_logs(user: User):
-  print(query_logs())
-  logs = []
-  for tupla in query_logs():
-    logs = format_query_logs(tupla, logs)
-  return {
-    'status': 'ok', 
-    'data': logs
-  }
+  return {'status': 'ok', 'logs': [{'logs': log.to_dict(), 'user': user.to_dict()} for log, user in query_logs()]}
 
 
-def query_logs() -> list[Log]:
+def query_logs() -> list[tuple[Log, User]]:
   with Session() as session:
-    return (
-      session.query(Log, User)
-      .outerjoin(User, Log.user_id == User.id)
-      .order_by(Log.created_at.desc())
-      .limit(300)
-      .all()
-    )
-
-
-def format_query_logs(
-  tupla: tuple[Log, User],
-  list: list[dict]
-) -> list[dict]:
-  for element in list:
-    print(element)
-  
-  output = {
-    **tupla[0].to_dict(),
-    'user': tupla[1].format_user()
-  }
-    
-  return list
+    return session.query(Log, User).join(User, Log.user_id == User.id).order_by(Log.created_at.desc()).limit(300).all()
