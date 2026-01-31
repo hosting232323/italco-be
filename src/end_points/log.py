fc2497a8f3167a1c62f40f8d4f9c4610827e3b72
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-01-31 15:32:19 +0100

    push

--- src/end_points/log.py
+++ src/end_points/log.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from ..database.enum import UserRole
 from .users.session import flask_session_authentication
 from ..database.schema import User, Log
@@ -8,12 +8,30 @@ from database_api import Session
 log_bp = Blueprint('log_bp', __name__)
 
 
-@log_bp.route('', methods=['GET'])
+@log_bp.route('', methods=['POST'])
 @flask_session_authentication([UserRole.ADMIN])
 def get_logs(user: User):
-  return {'status': 'ok', 'logs': [{'logs': log.to_dict(), 'user': user.to_dict()} for log, user in query_logs()]}
+  return {
+    'status': 'ok', 
+    'logs': [{'logs': log.to_dict(), 'user': user.to_dict()} for log, user in query_logs(request.json['filters'])]
+  }
 
 
-def query_logs() -> list[tuple[Log, User]]:
+def query_logs(filters: list) -> list[tuple[Log, User]]:
   with Session() as session:
-    return session.query(Log, User).join(User, Log.user_id == User.id).order_by(Log.created_at.desc()).limit(300).all()
+    query = (
+      session.query(Log, User)
+      .join(User, Log.user_id == User.id)
+    )
+    
+    for filter in filters:
+      model = globals()[filter['model']]
+      field = getattr(model, filter['field'])
+      value = filter['value']
+      
+      if model == User:
+        query = query.filter(field == value)
+      
+    query = query.order_by(Log.created_at.desc())
+    query = query.limit(300)
+    return query.all()
