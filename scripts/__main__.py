dfadcb56686b1c6fdfec48d1289a7cafaf18a462
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-02-02 16:01:20 +0100

    push

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,7 +1,15 @@
+import os
 import pandas as pd
+from sqlalchemy import and_
+from src.database.schema import ServiceUser
+from database_api import set_database, Session
+from database_api.operations import update, create
 
 
-if __name__ == '__main__':
+user_ids = [1,2,3,4]
+
+
+def get_excel_data():
   df = pd.read_excel('Listino Coppola est.xls')
 
   col_codice = df.columns[1]
@@ -11,12 +19,43 @@ if __name__ == '__main__':
   df[col_id_servizi] = pd.to_numeric(df[col_id_servizi], errors='coerce')
 
   df_validi = df.dropna(subset=[col_codice, col_id_servizi])
-  risultato = [
+  return [
     {
-      'codice': int(row[col_codice]),
-      'id_servizio': int(row[col_id_servizi])
+      'code': int(row[col_codice]),
+      'service_id': int(row[col_id_servizi])
     }
     for _, row in df_validi.iterrows()
   ]
 
-  print(risultato)
+
+def get_service_user(user_id: int, service_id: int) -> ServiceUser:
+  with Session() as session:
+    return session.query(ServiceUser).filter(
+      and_(
+        ServiceUser.user_id == user_id,
+        ServiceUser.service_id == service_id
+      )
+    ).first()
+
+
+if __name__ == '__main__':
+  set_database(os.environ['DATABASE_URL'])
+  
+  datas = get_excel_data()
+  created = []
+  for user in user_ids:
+    for data in datas:
+      service_user = get_service_user(user, data['service_id'])
+      print(service_user)
+      if not service_user:
+        created.append(create(ServiceUser, {
+          'code': data['code'],
+          'price': 10,
+          'user_id': user,
+          'service_id': data['service_id']
+        }))
+      else:
+        update(service_user, {
+          'code': data['code']
+        })
+  print(created)
