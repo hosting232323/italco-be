7b9be1802976bf29315856cb225762dbd1908fdb
Giovanni Colasanto <X1068076@guest.telecomitalia.it>
2026-02-04 04:11:49 +0100

    add price in script

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -13,15 +13,18 @@ def get_excel_data():
   df = pd.read_excel('/home/gralogic/Scrivania/TestWorkSpace/italco-be/listino euronics con id.xlsx')
 
   col_codice = df.columns[1]
+  col_prezzo = df.columns[3]
   col_id_servizi = df.columns[6]
-  
+
   df[col_codice] = pd.to_numeric(df[col_codice], errors='coerce')
+  df[col_prezzo] = pd.to_numeric(df[col_prezzo], errors='coerce')
   df[col_id_servizi] = pd.to_numeric(df[col_id_servizi], errors='coerce')
 
-  df_validi = df.dropna(subset=[col_codice, col_id_servizi])
+  df_validi = df.dropna(subset=[col_codice, col_prezzo, col_id_servizi])
   return [
     {
       'code': int(row[col_codice]),
+      'price': float(row[col_prezzo]),
       'service_id': int(row[col_id_servizi])
     }
     for _, row in df_validi.iterrows()
@@ -40,21 +43,24 @@ def get_service_user(user_id: int, service_id: int) -> ServiceUser:
 
 if __name__ == '__main__':
   set_database(os.environ['DATABASE_URL'])
-  
+
   datas = get_excel_data()
   created = []
   for user in user_ids:
     for data in datas:
       service_user = get_service_user(user, data['service_id'])
+      payload = {
+        'code': data['code'],
+        'price': data['price']
+      }
+
       if not service_user:
-        created.append(create(ServiceUser, {
-          'code': data['code'],
-          'price': 10,
+        payload.update({
           'user_id': user,
           'service_id': data['service_id']
-        }))
-      else:
-        update(service_user, {
-          'code': data['code']
         })
+        created.append(create(ServiceUser, payload))
+      else:
+        update(service_user, payload)
+
   print(created)
