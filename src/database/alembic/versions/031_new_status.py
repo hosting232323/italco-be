5b3e8bd56044a5122ace38e95f456bdbb8c14a41
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-03-14 12:26:24 +0100

    push

--- src/database/alembic/versions/031_new_status.py
+++ src/database/alembic/versions/031_new_status.py
@@ -5,6 +5,7 @@ Revises: 030
 Create Date: 2026-03-14 12:23:21.486798
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -18,16 +19,82 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column('order', sa.Column('confirmed', sa.Boolean(), nullable=True))
-    op.alter_column('order', 'external_status',
-               existing_type=postgresql.ENUM('NEW', 'CONFIRMED', 'BOOKING', 'DELIVERED', 'NOT_DELIVERED', 'REDELIVERY', 'REPLACEMENT', 'CANCELLED', 'URGENT', 'VERIFICATION', 'CANCELLED_TO_BE_REFUNDED', 'DELETED', 'AT_WAREHOUSE', 'TO_RESCHEDULE', name='orderstatus'),
-               type_=sa.Enum('NEW', 'CONFIRMED', 'BOOKING', 'DELIVERED', 'NOT_DELIVERED', 'REDELIVERY', 'REPLACEMENT', 'CANCELLED', 'URGENT', 'VERIFICATION', 'CANCELLED_TO_BE_REFUNDED', 'DELETED', name='euronicsstatus'),
-               existing_nullable=True)
+  op.add_column('order', sa.Column('confirmed', sa.Boolean(), nullable=True))
+  op.alter_column(
+    'order',
+    'external_status',
+    existing_type=postgresql.ENUM(
+      'NEW',
+      'CONFIRMED',
+      'BOOKING',
+      'DELIVERED',
+      'NOT_DELIVERED',
+      'REDELIVERY',
+      'REPLACEMENT',
+      'CANCELLED',
+      'URGENT',
+      'VERIFICATION',
+      'CANCELLED_TO_BE_REFUNDED',
+      'DELETED',
+      'AT_WAREHOUSE',
+      'TO_RESCHEDULE',
+      name='orderstatus',
+    ),
+    type_=sa.Enum(
+      'NEW',
+      'CONFIRMED',
+      'BOOKING',
+      'DELIVERED',
+      'NOT_DELIVERED',
+      'REDELIVERY',
+      'REPLACEMENT',
+      'CANCELLED',
+      'URGENT',
+      'VERIFICATION',
+      'CANCELLED_TO_BE_REFUNDED',
+      'DELETED',
+      name='euronicsstatus',
+    ),
+    existing_nullable=True,
+  )
 
 
 def downgrade() -> None:
-    op.alter_column('order', 'external_status',
-               existing_type=sa.Enum('NEW', 'CONFIRMED', 'BOOKING', 'DELIVERED', 'NOT_DELIVERED', 'REDELIVERY', 'REPLACEMENT', 'CANCELLED', 'URGENT', 'VERIFICATION', 'CANCELLED_TO_BE_REFUNDED', 'DELETED', name='euronicsstatus'),
-               type_=postgresql.ENUM('NEW', 'CONFIRMED', 'BOOKING', 'DELIVERED', 'NOT_DELIVERED', 'REDELIVERY', 'REPLACEMENT', 'CANCELLED', 'URGENT', 'VERIFICATION', 'CANCELLED_TO_BE_REFUNDED', 'DELETED', 'AT_WAREHOUSE', 'TO_RESCHEDULE', name='orderstatus'),
-               existing_nullable=True)
-    op.drop_column('order', 'confirmed')
+  op.alter_column(
+    'order',
+    'external_status',
+    existing_type=sa.Enum(
+      'NEW',
+      'CONFIRMED',
+      'BOOKING',
+      'DELIVERED',
+      'NOT_DELIVERED',
+      'REDELIVERY',
+      'REPLACEMENT',
+      'CANCELLED',
+      'URGENT',
+      'VERIFICATION',
+      'CANCELLED_TO_BE_REFUNDED',
+      'DELETED',
+      name='euronicsstatus',
+    ),
+    type_=postgresql.ENUM(
+      'NEW',
+      'CONFIRMED',
+      'BOOKING',
+      'DELIVERED',
+      'NOT_DELIVERED',
+      'REDELIVERY',
+      'REPLACEMENT',
+      'CANCELLED',
+      'URGENT',
+      'VERIFICATION',
+      'CANCELLED_TO_BE_REFUNDED',
+      'DELETED',
+      'AT_WAREHOUSE',
+      'TO_RESCHEDULE',
+      name='orderstatus',
+    ),
+    existing_nullable=True,
+  )
+  op.drop_column('order', 'confirmed')
