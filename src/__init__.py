0ae27caec53b7abaa3c58d70a076f42911b62cd2
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2026-04-20 18:47:00 +0200

    fix folder postgres backup

--- src/__init__.py
+++ src/__init__.py
@@ -55,7 +55,7 @@ def serve_image(filename):
 @error_catching_decorator
 @swagger_decorator
 def trigger_backup():
-  return db_backup(DATABASE_URL, os.path.join(BACKUP_FOLDER, 'postgres-backup'), 'local')
+  return db_backup(DATABASE_URL, os.path.join(BACKUP_FOLDER, 'postgres-backup'), 'local', 'postgres-backup')
 
 
 @app.route('/folder-backup', methods=['GET'])
