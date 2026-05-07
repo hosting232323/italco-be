62fcb9137684ee454576423531295aa4e7f5c0e5
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-05-07 21:22:02 +0200

    new-chatty

--- src/end_points/chatty.py
+++ src/end_points/chatty.py
@@ -17,7 +17,7 @@ assistant_id = os.environ['ASSISTANT_ID']
 chatty_bp = Blueprint('chatty_bp', __name__)
 
 
-@chatty_bp.route('message', methods=['POST'])
+@chatty_bp.route('chat', methods=['POST'])
 @flask_session_authentication([UserRole.ADMIN, UserRole.OPERATOR, UserRole.CUSTOMER, UserRole.DELIVERY])
 def send_message(user: User):
   if 'thread_id' in request.json and request.json['thread_id']:
