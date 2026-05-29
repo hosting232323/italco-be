b6d07c6c3428dc016fcf48bbbfe4a7e24ba767a7
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-05-29 16:42:50 +0200

    push

--- src/end_points/orders/photo.py
+++ src/end_points/orders/photo.py
@@ -25,9 +25,7 @@ def handle_photos(data: dict, order: Order, session: session_type):
             'link': get_base_photo_path()
             + os.path.basename(
               upload_file(
-                uploaded_file,
-                f'{id}{guess_extension(uploaded_file.mimetype)}',
-                STATIC_FOLDER, 'local', 'photos'
+                uploaded_file, f'{id}{guess_extension(uploaded_file.mimetype)}', STATIC_FOLDER, 'local', 'photos'
               )
             ),
           },
