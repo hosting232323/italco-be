7f2b251a638b82755f85c74893964a6030cda544
Giovanni Colasanto <X1068076@guest.telecomitalia.it>
2026-06-15 18:33:54 +0200

    fix di vanni

--- src/end_points/schedule/utils.py
+++ src/end_points/schedule/utils.py
@@ -57,6 +57,8 @@ def format_schedule_data(schedule_data: dict, session=None):
           break
 
   users = schedule_data['users']
+  if not orders or not users or len(users) == 0:
+    return None, None, None, {'status': 'ko', 'error': 'Errore nella creazione del borderò'}
 
   del schedule_data['users']
   del schedule_data['schedule_items']
