4fc794d2433fb19b7090fbc50b1a67cbe78e2046
Vanni <hosting23232323@gmail.com>
2026-06-18 07:04:35 +0000

    fix-rae-exportation

--- src/end_points/exportation/__init__.py
+++ src/end_points/exportation/__init__.py
@@ -4,11 +4,11 @@ from ...database.schema import User
 from ...database.enum import UserRole
 from ..users.session import flask_session_authentication
 
-from .rae import export_rae
 from .order import export_order
 from .schedule import export_schedule
 from .excel import export_orders_excel
 from .invoice import export_order_invoice
+from .rae import export_rae, export_rae_by_product
 
 
 export_bp = Blueprint('export_bp', __name__)
@@ -36,6 +36,12 @@ def export_orders_schedule(user: User, id):
 @flask_session_authentication([UserRole.ADMIN, UserRole.OPERATOR])
 def export_orders_rae(user: User, order_id):
   return export_rae(user, order_id)
+
+
+@export_bp.route('rae/product/<rae_product_id>', methods=['GET'])
+@flask_session_authentication([UserRole.ADMIN, UserRole.OPERATOR])
+def export_rae_product(user: User, rae_product_id):
+  return export_rae_by_product(user, int(rae_product_id))
 
 
 @export_bp.route('orders/excel', methods=['POST'])
