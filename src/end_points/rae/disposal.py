3e8e266446519e771c545d737eab3e8be35783a6
cl_cassanelli <cldevofficial@gmail.com>
2026-06-19 23:18:26 +0200

    push

--- src/end_points/rae/disposal.py
+++ src/end_points/rae/disposal.py
@@ -5,14 +5,15 @@ from ...database.schema import Disposal, Carrier, CollectionCenter, RaeProduct
 
 
 def create_rae_disposal(data: dict):
+  for rp in get_by_ids(RaeProduct, data['rae_product_ids']):
+    update(rp, {'disposal_id': id, 'status': RaeStatus.DISPOSED_OFF})
+  data.pop('rae_product_ids', None)
   create(Disposal, data)
   return {'status': 'ok', 'message': 'Operazione completata!'}
 
 
 def update_rae_disposal(id: int, data: dict):
   update(get_by_id(Disposal, id), {'document_fir': data['document_fir']})
-  for rp in get_by_ids(RaeProduct, data['rae_product_ids']):
-    update(rp, {'disposal_id': id, 'status': RaeStatus.DISPOSED_OFF})
   return {'status': 'ok', 'message': 'Operazione completata!'}
 
 
