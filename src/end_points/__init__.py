ff834c1d12cbec75d58ee5afbfaf222846ac51ea
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2026-07-04 16:00:26 +0200

    Avoid circular session auth import

--- src/end_points/__init__.py
+++ src/end_points/__init__.py
@@ -1,7 +1,12 @@
 from api.users import build_session_authentication
 
 from .. import STATIC_FOLDER
-from .users.queries import get_user_by_nickname
+
+
+def get_user_by_nickname(nickname: str):
+  from .users.queries import get_user_by_nickname as query_user_by_nickname
+
+  return query_user_by_nickname(nickname)
 
 
 flask_session_authentication = build_session_authentication(
