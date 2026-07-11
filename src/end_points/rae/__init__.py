82cda8347f8069b756e8ff7b74702077fb9a6c2c
cl_cassanelli <cldevofficial@gmail.com>
2026-07-11 16:36:19 +0200

    push

--- src/end_points/rae/__init__.py
+++ src/end_points/rae/__init__.py
@@ -65,9 +65,7 @@ def update_product(_, id):
   with Session() as session:
     update_rae_product(
       int(id),
-      handle_document(
-        json.loads(request.form.get('data')), 'rae/dtr-documents', 'rae_product', 'link'
-      ),
+      handle_document(json.loads(request.form.get('data')), 'rae/dtr-documents', 'rae_product', 'link'),
       session=session,
     )
     session.commit()
@@ -142,20 +140,12 @@ def update_disposal(_, id):
 
     if 'first_copy_document_fir' in request.files:
       data = handle_document_by_name(
-        data,
-        'rae/first-copy-fir-documents',
-        'disposal',
-        'first_copy_document_fir',
-        'first_copy_document_fir'
+        data, 'rae/first-copy-fir-documents', 'disposal', 'first_copy_document_fir', 'first_copy_document_fir'
       )
 
     if 'fourth_copy_document_fir' in request.files:
       data = handle_document_by_name(
-        data,
-        'rae/fourth-copy-fir-documents',
-        'disposal',
-        'fourth_copy_document_fir',
-        'fourth_copy_document_fir'
+        data, 'rae/fourth-copy-fir-documents', 'disposal', 'fourth_copy_document_fir', 'fourth_copy_document_fir'
       )
 
     update_rae_disposal(int(id), data, session=session)
--- src/end_points/rae/document.py
+++ src/end_points/rae/document.py
@@ -17,9 +17,7 @@ def handle_document(data: dict, folder: str, model: str, field_name: str) -> dic
   return data
 
 
-def handle_document_by_name(
-  data: dict, folder: str, model: str, field_name: str, file_name: str
-) -> dict:
+def handle_document_by_name(data: dict, folder: str, model: str, field_name: str, file_name: str) -> dict:
   uploaded_file = request.files.get(file_name)
   if not uploaded_file or uploaded_file.mimetype != 'application/pdf':
     return data
